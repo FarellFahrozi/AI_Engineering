c43d626619420d143d9ae0f920a2cb069fd7c5fe
FarellFahrozi <farellfahroziofficial@gmail.com>
2025-01-25 01:03:22 +0700

    code file

---
+++ week1_python_programming_basics/data_types.py
@@ -0,0 +1,22 @@
+#Integers and floats
+age = 25
+height = 5.9
+
+#Strings
+name = "Alice"
+greeting = "Hello, " + name
+
+#List and tuple
+numbers = [1,2,3,4]
+name = ["Alice", "Jack"]
+coordinates = (10, 20)
+
+#Dictionary
+person = {"name": "Alice", "age": 25}
+
+#Booleans
+is_student = False
+
+#Print output
+print(numbers)
+print(name)
