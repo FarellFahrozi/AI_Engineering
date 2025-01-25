61370bc84c002ccb802c9d6f71f0811376010049
FarellFahrozi <farellfahroziofficial@gmail.com>
2025-01-25 15:22:24 +0700

    first commit

---
+++ week1_python_programming_basics/day1_development_setup/day1_exercise2.py
@@ -0,0 +1,20 @@
+# Mendefinisikan variabel dengan tipe data berbeda
+integer_var = 10
+float_var = 3.14
+string_var = "AI"
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+dict_var = {"name": "Alice", "role":"Engineer"}
+bool_var = True
+
+
+# Print dan manipulasi variable
+print("Integer: ", integer_var)
+print("Float: ", float_var)
+print("String: ", string_var + " Bootcamp")
+list_var.append(4)
+print("List: ", list_var)
+print("Tupple: ", tuple_var)
+print("Dictionary Value Name: ", dict_var['name'])
+print("Dictionary Value Role: ", dict_var['role'])
+print("Boolean: ", bool_var)
