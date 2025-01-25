313b51f3b3f13391a808f4b6cffc0cc7e7a680f6
FarellFahrozi <farellfahroziofficial@gmail.com>
2025-01-26 00:24:09 +0700

    first commit

---
+++ week1_python_programming_basics/day2_control_flow/day2_1.py
@@ -0,0 +1,53 @@
+# Example 1: checking a condition
+# num = -50
+# if num > 0:
+#     print("Positive Number")
+# elif num == 0:
+#     print("Zero Number")
+# else:
+#     print("Negative Numver")
+    
+    
+# Example 2: Nested Condition
+# age = 28
+# if age > 18:
+#     if age < 30:
+#         print("Young Adult")
+#     else:
+#         print("Adult")
+
+
+
+#----------------------------------------------------------------#
+# Syntax for-loop
+
+# for item in sequence:
+#     #Code block
+
+
+# Loop dengan list
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+    
+# Loop dengan range
+# for i in range(5):#[0,1,2,3,4]
+#     print(i)
+
+
+#----------------------------------------------------------------#
+# Syntax while-loop
+
+# while condition:
+#     #Code block
+
+
+# Count down 5
+# count = 5
+# while count > 0:
+#     print(count)
+#     count -= 1
+
+# print("Outside While Loop")
+
+
